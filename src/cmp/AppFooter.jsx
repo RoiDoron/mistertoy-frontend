ffb0cb74438fbad7bf95f0650bce10e7c0b29d7f
Roi Doron <roidoron6@gmail.com>
2024-04-01 00:00:50 +0300

    Edit app footer

--- src/cmp/AppFooter.jsx
+++ src/cmp/AppFooter.jsx
@@ -4,7 +4,6 @@ import { UserMsg } from "./UserMsg.jsx";
 export function AppFooter(){
     return (
         <footer className='app-footer'>
-            <h3>Hi im an awesome footer</h3>
             <h3>Created by Roi Doron</h3>
             <UserMsg/>
         </footer>
