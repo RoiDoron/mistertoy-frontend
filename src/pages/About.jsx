ddd84d6978c42ec0affc19a22e9e408622679807
Roi Doron <roidoron6@gmail.com>
2024-03-31 23:59:42 +0300

    Edit the about

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,6 +2,8 @@
 
 export function About(){
     return <section className="about-container">
-        <div>Im about this all thing</div>
+        <h1>Who are we?</h1>
+        <p>We are a <span>toys</span> lovers that dream about <span>toys</span> play with <span>toys</span> and think about <span>toys</span> all the time! </p>
+        <img src="/src/assets/img/about.jpg" alt="" />
     </section>
 }
