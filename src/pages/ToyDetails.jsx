560108e9727201a4dd5df5bfb753cbba52abdd95
Roi Doron <roidoron6@gmail.com>
2024-04-01 00:00:26 +0300

    Edit the toy details

--- src/pages/ToyDetails.jsx
+++ src/pages/ToyDetails.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react"
-import { Navigate, useParams } from "react-router-dom"
+import { Link, Navigate, useParams } from "react-router-dom"
 import { toyService } from "../services/toy.service.js"
 
 
@@ -24,7 +24,18 @@ export function ToyDetails() {
     if (!toy) return <div>Loading...</div>
     return <section className="details-container">
         <h1>{toy.name}</h1>
-        <h5>price: ${toy.price}</h5>
-        <h5>{toy.inStock ? 'In stock!':'Out of stock !'}</h5>
+        <main className=" ">
+        
+        <img src={`https://robohash.org/${toy._id}?set=set4`} alt="the toy picture" />
+        <div className="content-container">
+        <h3>price: ${toy.price}</h3>
+        <h3 className={toy.inStock ? 'in-stock':'out-of-stock'}>{toy.inStock ? 'In stock!':'Out of stock..'}</h3>
+        <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi voluptas
+        cumque tempore, aperiam sed dolorum rem!
+      </p>
+        </div>
+        </main>
+        <button><Link to={`/toy`}>Back</Link></button>
     </section>
 }
